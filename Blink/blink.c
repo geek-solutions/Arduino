e0dee38355f5a06501d3202a9c7f42e5393317d4
Johannes Augustin <ja@geek-solutions.net>
2016-04-06 00:36:20 +0200

    adding folder Blink after deleting it

---
+++ Blink/blink.c
@@ -0,0 +1,27 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define var 1000 
+
+char main (void) {
+
+	/*
+	* Set 5th bit of Register DDRB to 1 
+	* to configure PB5 of the chip as output
+	* PB5 equals ArduinoPin13
+	* reference: https://www.arduino.cc/en/Hacking/PinMapping168
+	*/
+	DDRB |= (1<<PB5);
+
+	/*
+	* Loop to toggle 5th bit ot PORTB
+	* ^= bitwise exclusive OR
+	*/
+
+	while (1) {
+		PORTB ^= (1<<PB5);
+		_delay_ms(var);
+	}
+
+return 1;
+}
