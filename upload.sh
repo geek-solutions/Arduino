f9bcf5099c8df081d2ef0967c9854d674486278b
Johannes Augustin <ja@geek-solutions.net>
2016-04-06 00:52:49 +0200

    added comments

--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-avrdude -p atmega328p -P /dev/ttyACM0 -c stk500v1 -b 115200 -F -u -U flash:w:main.hex
+# avrdude should be installed
+# fou.hex should be renamed with your actual name
+
+avrdude -p atmega328p -P /dev/ttyACM0 -c stk500v1 -b 115200 -F -u -U flash:w:foo.hex
